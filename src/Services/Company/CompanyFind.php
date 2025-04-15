f1e8f7a7160b9d0fe2678faff9deca4d4684592c
ericNKS <ek.silva.santos@gmail.com>
2025-04-15 16:04:33 -0300

    fix: exclude soft-deleted companies from CompanyFind

--- src/Services/Company/CompanyFind.php
+++ src/Services/Company/CompanyFind.php
@@ -13,6 +13,10 @@ class CompanyFind
 
     public function execute(int $id): ?Company {
         $company = $this->repository->find($id);
+
+        if (!is_null($company->getDeletedAt())) {
+            return null;
+        }
         return $company;
     }
 }
