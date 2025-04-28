f72e0319b0fe1520ad1b911ade561fa3622fab4c
ericNKS <ek.silva.santos@gmail.com>
2025-04-28 18:20:05 -0300

    feat: finish login and register user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,24 +2,29 @@
 
 namespace App\Controller;
 
+use App\DTO\AuthRequest;
 use App\Entity\Company;
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\UserRegisterType;
 use App\Repository\UserRepository;
+use App\Services\User\UserAuth;
 use App\Services\User\UserCreate;
 use App\Services\User\UserFind;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/user')]
+#[Route('/api')]
 final class UserController extends AbstractController
 {
-    #[Route('/{id}', name: 'find_user', methods: ['GET'])]
+    #[Route('/user/{id}', name: 'find_user', methods: ['GET'])]
     public function find(
         int $id,
         UserRepository $repository,
@@ -37,21 +42,60 @@ final class UserController extends AbstractController
         return $this->json($user, Response::HTTP_OK);
     }
 
-    #[Route('', name: 'create_user', methods: ['POST'])]
-    public function store(
+    #[Route('/register', name: 'register_user', methods: ['POST'])]
+    public function register(
         Request $request,
         EntityManagerInterface $em,
         FormFactoryInterface $formFactory,
         UserRepository $repository,
+        UserPasswordHasherInterface $passwordHasher
     ): Response
     {
         $body = json_decode($request->getContent(), true);
 
         $findService = new UserFind($repository);
 
-        $userCreateService = new UserCreate($em, $formFactory, $findService);
+        $userCreateService = new UserCreate($em, $formFactory, $findService, $passwordHasher);
         $user = $userCreateService->execute($body);
 
-        return $this->json($user, Response::HTTP_CREATED);
+        $userAuth = new AuthRequest([
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword(),
+        ]);
+
+        $userFind = new UserFind($repository);
+        $auth = new UserAuth($userFind);
+
+        $token = $auth->execute($userAuth);
+
+        return $this->json([
+            'user' => $user,
+            'token' => $token,
+        ], Response::HTTP_CREATED);
+    }
+
+    #[Route('/login', name: 'login_user', methods: ['POST'])]
+    public function login(
+        Request $request,
+        FormFactoryInterface $formFactory,
+        UserRepository $repository,
+        UserPasswordHasherInterface $passwordHasher,
+        JWTTokenManagerInterface $jwtManager
+    ): Response
+    {
+        $body = json_decode($request->getContent(), true);
+
+        $authRequest = new AuthRequest($body);
+        $userFind = new UserFind($repository);
+
+        $userAuth = new UserAuth($userFind, $passwordHasher);
+
+        $user = $userAuth->execute($authRequest);
+
+        $token = $jwtManager->create($user);
+
+        return $this->json([
+            'token' => $token,
+        ], Response::HTTP_CREATED);
     }
 }
--- src/Services/User/UserCreate.php
+++ src/Services/User/UserCreate.php
@@ -2,14 +2,14 @@
 
 namespace App\Services\User;
 
-use App\Entity\Company;
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\UserRegisterType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCreate
 {
@@ -17,16 +17,19 @@ class UserCreate
         private EntityManagerInterface $em,
         private FormFactoryInterface $formFactory,
         private UserFind $userFind,
+        private UserPasswordHasherInterface $passwordHasher,
     ){}
 
-    public function execute(array $userData): ?Company
+    public function execute(array $userData): ?User
     {
         $user = new User();
 
-        $form = $this->formFactory->create(UserType::class, $user);
+        $form = $this->formFactory->create(UserRegisterType::class, $user);
         $form->submit($userData);
 
         $this->isValid($form);
+
+        $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
 
         $this->em->persist($user);
         $this->em->flush();
